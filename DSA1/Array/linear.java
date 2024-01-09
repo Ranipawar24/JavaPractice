0541d92363db6d307002b7ea3c4e380eb6599bce
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-09 11:53:56 +0530

    Update linear.java

--- DSA1/Array/linear.java
+++ DSA1/Array/linear.java
@@ -1,10 +1,16 @@
 package DSA1.Array;
 
 public class linear {
+    // defining array
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.print(i + " ");
-        }
+        int marks[] = new int[3];
+        marks[0] = 85;// phy
+
+        marks[1] = 89;// math
+        marks[2] = 90;// sci
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
 
     }
 }
