7dc5826d27b9f16e982c1e46c47c7fa9c958bdc1
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-09 14:35:59 +0530

    Update linear.java

--- DSA1/Array/linear.java
+++ DSA1/Array/linear.java
@@ -1,8 +1,11 @@
 package DSA1.Array; // its a folder which my java files lies
 
+import java.util.Scanner;
+
 public class linear {
     // defining array
     public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
         int marks[] = new int[3];
         marks[0] = 85;// phy
 
