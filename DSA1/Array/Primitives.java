d09faa3d0e443839d7f6ae6ea0346927e7a62b44
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-09 14:41:29 +0530

    Create Primitives.java

---
+++ DSA1/Array/Primitives.java
@@ -0,0 +1,13 @@
+package DSA1.Array;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 46;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largDeciNum = 45852116.4587;
+        long largeInt = 235784544554455L;
+        boolean check = false;
+
+    }
+}
