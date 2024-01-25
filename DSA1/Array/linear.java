41768e4634a1b7a30846b547b2547dee10378bdc
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-25 16:55:02 +0530

    Update linear.java

--- DSA1/Array/linear.java
+++ DSA1/Array/linear.java
@@ -1,39 +1,48 @@
 package DSA1.Array; // its a folder which my java files lies
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+// STRINGS ARE IMMUTABLE IN JAVA BCZ ITS CHANGE IN OBJECTS 
+// AND ARRAYS ARE MUTTABLE IN JAVA WE CAN CHANGE THE OBJECTS
 public class linear {
     // defining array
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int marks[] = new int[5];
+        // int marks[] = new int[5];
 
-        // THIS IS ARRAY OF PREMITIVES
-        // marks[0] = 85;// phy
+        // // THIS IS ARRAY OF PREMITIVES
+        // // marks[0] = 85;// phy
 
-        // marks[1] = 89;// math
-        // marks[2] = 90;// sci
-        // System.out.println(marks[0]);
-        // System.out.println(marks[1]);
-        // System.out.println(marks[2]);
+        // // marks[1] = 89;// math
+        // // marks[2] = 90;// sci
+        // // System.out.println(marks[0]);
+        // // System.out.println(marks[1]);
+        // // System.out.println(marks[2]);
 
-        // input using for loop
-
-        for (int i = 0; i < marks.length; i++) {
-            marks[i] = sc.nextInt();
-        }
+        // // input using for loop
 
         // for (int i = 0; i < marks.length; i++) {
-        // System.out.print(marks[i] + " ");
+        // marks[i] = sc.nextInt();
         // }
 
-        // input using foreach loop
-        for (int num : marks) { // for every element in the marks , print the marks
-            System.out.print(num + " "); // here num represents element of the marks
-        }
-        System.out.println(marks[6]);
+        // // for (int i = 0; i < marks.length; i++) {
+        // // System.out.print(marks[i] + " ");
+        // // }
 
-        // THIS IS ARRAY OF OBJECTS
+        // // input using foreach loop
+        // for (int num : marks) { // for every element in the marks , print the marks
+        // System.out.print(num + " "); // here num represents element of the marks
+        // }
+        // System.out.println(marks[6]);
+
+        // // THIS IS ARRAY OF OBJECTS
+
+        // String str[] = new String[4];
+        // for (int i = 0; i < str.length; i++) {
+        // str[i] = sc.next();
+        // }
+        // System.out.println(Arrays.toString(str));
     }
 
 }
