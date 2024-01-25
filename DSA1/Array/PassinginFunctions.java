cff73adb53edb7b5be24597364688823b4ad3a31
Rani Pawar <pawar.rani241996@gmail.com>
2024-01-25 17:00:43 +0530

    Create PassinginFunctions.java

---
+++ DSA1/Array/PassinginFunctions.java
@@ -0,0 +1,17 @@
+package DSA1.Array;
+
+import java.util.Arrays;
+
+// ARRAYS IS MUTTABLE BECAUSE ITS CHANGEING OBJECTS
+public class PassinginFunctions {
+    public static void main(String[] args) {
+        int nums[] = { 2, 8, 78, 63 };
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void change(int arr[]) {
+        arr[0] = 85;
+    }
+}
