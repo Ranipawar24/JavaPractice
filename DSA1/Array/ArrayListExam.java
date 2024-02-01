75de1ad8c6ab9df3b12f09314ed90d54cad68dfd
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-01 13:30:54 +0530

    Create ArrayListExam.java

---
+++ DSA1/Array/ArrayListExam.java
@@ -0,0 +1,12 @@
+package DSA1.Array;
+
+import java.util.ArrayList;
+
+public class ArrayListExam {
+    // Syntax
+    // ArrayList<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+    }
+}
