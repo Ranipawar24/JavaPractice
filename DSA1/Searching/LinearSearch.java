2636dfceb883408e0986bc83edaafb174ef4bbd8
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-03 15:13:45 +0530

    Update LinearSearch.java

--- DSA1/Searching/LinearSearch.java
+++ DSA1/Searching/LinearSearch.java
@@ -2,6 +2,8 @@ package DSA1.Searching;
 
 public class LinearSearch {
     public static void main(String[] args) {
+        // TIME COMPLEXCITY = O(N)
+        // SPACE COMPLEXCITY = O(1)
 
     }
 }
