50d7a31a1357321cc6d4b5c3f5794c8ad0564938
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-05 18:25:56 +0530

    Update LinearSearch.java

--- DSA1/Searching/LinearSearch.java
+++ DSA1/Searching/LinearSearch.java
@@ -1,9 +1,12 @@
 package DSA1.Searching;
 
 public class LinearSearch {
+    // 1. Search the array: return the index if item found otherwise if item not
+    // found return -1
     public static void main(String[] args) {
         // TIME COMPLEXCITY = O(N)
         // SPACE COMPLEXCITY = O(1)
 
     }
+
 }
