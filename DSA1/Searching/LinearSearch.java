01a1e312a6801efe42f68c0d706ab7816d09627c
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-07 12:12:02 +0530

    Update LinearSearch.java

--- DSA1/Searching/LinearSearch.java
+++ DSA1/Searching/LinearSearch.java
@@ -6,13 +6,24 @@ public class LinearSearch {
     public static void main(String[] args) {
         // TIME COMPLEXCITY = O(N)
         // SPACE COMPLEXCITY = O(1)
-        int[] arr = { 1, 25, 23, 2, 4, 8 };
+        int[] num = { 1, 25, 23, 2, 4, 8 };
         int target = 20;
+        int index = LinearSearch1(num, target);
+        if (index == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("key is at index: " + index);
+        }
 
     }
 
-    public static int LinearSearch1(int[] arr, int target) {
-        System.out.println(target);
+    public static int LinearSearch1(int[] num, int target) {
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == target) {
+                return i;
+            }
+        }
+        return -1;
 
     }
 
