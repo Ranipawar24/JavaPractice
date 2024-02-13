5f32d4d3087c503ba251a6ce5e152d2e9da7d8ea
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-13 12:49:56 +0530

    Update largestNumlinear.java

--- DSA1/Searching/largestNumlinear.java
+++ DSA1/Searching/largestNumlinear.java
@@ -2,6 +2,18 @@ package DSA1.Searching;
 
 public class largestNumlinear {
     public static void main(String[] args) {
+        int num[] = { 1, 33, 5, 8, 6, 9 };
+        System.out.println("largest value is : " + getLargest(num));
 
+    }
+
+    public static int getLargest(int num[]) {
+        int largest = Integer.MIN_VALUE; // -infinity
+        for (int i = 0; i < num.length; i++) {
+            if (largest < num[i]) {
+                largest = num[i];
+            }
+        }
+        return largest;
     }
 }
