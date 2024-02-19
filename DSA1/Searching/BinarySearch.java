82953851e301cb6b03a8684e84bb4b1575e15465
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-19 15:18:45 +0530

    Update BinarySearch.java

--- DSA1/Searching/BinarySearch.java
+++ DSA1/Searching/BinarySearch.java
@@ -19,6 +19,20 @@ public class BinarySearch {
     }
 
     public static int binarySearch(int num[], int key) {
+        int start = 0, end = num.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // comparison
+            if (num[mid] == key) {
+                return mid;
+            }
+            if (num[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
 
     }
 }
