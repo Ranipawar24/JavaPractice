2809071497f314ccc082130a4f91204c625f6482
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-19 15:07:13 +0530

    Update BinarySearch.java

--- DSA1/Searching/BinarySearch.java
+++ DSA1/Searching/BinarySearch.java
@@ -2,5 +2,15 @@ package DSA1.Searching;
 
 public class BinarySearch {
     // prerequisite - sorted arrays
+    // PSEUDO CODE
+    /*
+     * start= 0, end =-1
+     * while(start<=end)
+     * findmid
+     * compare mid & key
+     * mid== key Found
+     * mid> key Left 1st half
+     * mid< key Right 2nd half
+     */
 
 }
