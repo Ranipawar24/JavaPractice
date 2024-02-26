c34b86ae7f4fb18b4f43ac51afae074909784ff3
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-26 14:51:02 +0530

    Update ReverseanArray.java

--- DSA1/Searching/ReverseanArray.java
+++ DSA1/Searching/ReverseanArray.java
@@ -4,6 +4,9 @@ public class ReverseanArray {
     public static void main(String[] args) {
         int num[] = { 2, 4, 8, 10, 9 };
         reverse(num);
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i] + " ");
+        }
 
     }
 
