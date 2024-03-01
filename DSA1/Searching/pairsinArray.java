aa3c30e7efd4206138f126c9f9f4dc8af896a747
Rani Pawar <pawar.rani241996@gmail.com>
2024-03-01 17:59:54 +0530

    Update pairsinArray.java

--- DSA1/Searching/pairsinArray.java
+++ DSA1/Searching/pairsinArray.java
@@ -3,10 +3,6 @@ package DSA1.Searching;
 public class pairsinArray {
     public static void main(String[] args) {
         int num[] = { 2, 4, 8, 6, 7, 9 };
-        for (int i = 0; i < num; i++) {
-            //
-
-        }
 
     }
 
