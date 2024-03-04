39d8b2b8bf3dd2b3ea3e09cacf5beba52b13b455
Rani Pawar <pawar.rani241996@gmail.com>
2024-03-04 15:02:27 +0530

    Update pairsinArray.java

--- DSA1/Searching/pairsinArray.java
+++ DSA1/Searching/pairsinArray.java
@@ -10,7 +10,7 @@ public class pairsinArray {
     public static void printPairs(int num[]) {
         for (int i = 0; i < num.length; i++) {
             int curr = num[i];
-            for (int j = 0; j < num.length; j++) {
+            for (int j = i + 1; j < num.length; j++) {
                 System.out.print("(" + curr + "," + num[j] + ") ");
             }
             System.out.println();
