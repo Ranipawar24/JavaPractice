5dbabd86c0599e3e469706d30a5c2a4fd060ef57
Rani Pawar <pawar.rani241996@gmail.com>
2024-03-27 21:39:09 +0530

    Update MergeSort.java

--- DSA1/Sorting/MergeSort.java
+++ DSA1/Sorting/MergeSort.java
@@ -3,8 +3,6 @@ package DSA1.Sorting;
 public class MergeSort {
     public static void main(String[] args) {
 
-        System.out.println();
-
     }
 
     public static void mergearray(int si, int ei) {
