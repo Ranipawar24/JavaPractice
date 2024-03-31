a7bc52297710eee9da133d06afe76f5919c6a94d
Rani Pawar <pawar.rani241996@gmail.com>
2024-03-31 10:27:52 +0530

    Update Palidrome.java

--- DSA1/Palidrome.java
+++ DSA1/Palidrome.java
@@ -3,6 +3,7 @@ package DSA1;
 public class Palidrome {
     // check if number is palindrome
     public static void main(String[] args) {
+
         int n = 454;
         int rev = 0;
         int num = n;
