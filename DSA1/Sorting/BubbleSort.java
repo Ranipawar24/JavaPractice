ff00d238177f8e3b619303253084c368395a9780
Rani Pawar <pawar.rani241996@gmail.com>
2024-04-23 13:00:20 +0530

    Update BubbleSort.java

--- DSA1/Sorting/BubbleSort.java
+++ DSA1/Sorting/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
     public static void main(String[] args) {
 
         /*
-         * we do a certain no. of passes and in every passes we compare
+         * we do a certain no. of passes/ iteration and in every passes we compare
          * adjacent elements and swap them if they are not in correct order.
          */
 
