325df6da7fe6a6a106cb41346607339d44afa465
Rani Pawar <pawar.rani241996@gmail.com>
2024-05-16 12:41:30 +0530

    Update BubbleSort.java

--- DSA1/Sorting/BubbleSort.java
+++ DSA1/Sorting/BubbleSort.java
@@ -6,6 +6,9 @@ public class BubbleSort {
         /*
          * we do a certain no. of passes/ iteration and in every passes we compare
          * adjacent elements and swap them if they are not in correct order.
+         * 
+         * In every iteration the largest no. in part of array to be process gets its
+         * correct positions.
          */
 
     }
