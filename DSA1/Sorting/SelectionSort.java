630782df42e965dfa47e113d4cb8f750d817f4f8
Rani Pawar <pawar.rani241996@gmail.com>
2024-06-07 12:30:42 +0530

    Update SelectionSort.java

--- DSA1/Sorting/SelectionSort.java
+++ DSA1/Sorting/SelectionSort.java
@@ -2,8 +2,12 @@ package DSA1.Sorting;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        
+
         System.out.println();
+
+    }
+
+    public static void main(String[] args) {
 
     }
 
