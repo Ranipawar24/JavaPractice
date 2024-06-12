2b779912b75f22550d48b115525f60c88df882b5
Rani Pawar <pawar.rani241996@gmail.com>
2024-06-12 11:40:24 +0530

    Update SelectionSort.java

--- DSA1/Sorting/SelectionSort.java
+++ DSA1/Sorting/SelectionSort.java
@@ -3,14 +3,12 @@ package DSA1.Sorting;
 public class SelectionSort {
     public static void main(String[] args) { // Time complexcity = O(n²)
 
-        int[] arr = {7,4,5,1,2};
-        selectionSort(arr)
+        int[] arr = { 7, 4, 5, 1, 2 };
+        selectionSort(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
-            
-        }
+            System.out.print(arr[i] + " ");
 
-       
+        }
 
     }
 
