3f9db2a95f53434a47197a1ccd24d0e77fa78954
Rani Pawar <pawar.rani241996@gmail.com>
2024-06-27 15:18:14 +0530

    Update InsertionSort.java

--- DSA1/Sorting/InsertionSort.java
+++ DSA1/Sorting/InsertionSort.java
@@ -3,12 +3,9 @@ package DSA1.Sorting;
 public class InsertionSort {
     public static void main(String[] args) {
 
-        System.out.println();
-
     }
 
     public static void main(String[] args) {
-
 
     }
 }
