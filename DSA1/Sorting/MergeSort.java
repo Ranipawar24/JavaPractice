c1d7c9b25bfb594a2893148087ccf98e872d6926
Rani Pawar <pawar.rani241996@gmail.com>
2024-09-24 09:36:15 +0530

    Update MergeSort.java

--- DSA1/Sorting/MergeSort.java
+++ DSA1/Sorting/MergeSort.java
@@ -1,6 +1,10 @@
 package DSA1.Sorting;
 
 public class MergeSort {
+    // Divide and Conquer methode
+    // divide the array into 2 equal halves
+    // sort the 2 subarrays seperately using recursion
+    // merge the 2 sorted subarray to create an overall sorted arrays.
     public static void main(String[] args) {
 
         System.out.println();
