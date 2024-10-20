fe1205e1b62b917dd54bbf18500124ccfae27797
Rani Pawar <pawar.rani241996@gmail.com>
2024-10-20 13:07:31 +0530

    Update question1.java

--- DSA1/Array/problems/question1.java
+++ DSA1/Array/problems/question1.java
@@ -4,6 +4,9 @@ public class question1 {
     // 1. Given 2 integers a and b. swap the given values using temporary variables?
 
     public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        int[] arr = { 1, 2, 3, 4, 5 };
 
     }
 
