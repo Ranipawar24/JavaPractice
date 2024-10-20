e3f3eaaea326f72ec84b5432983e07b5383b158e
Rani Pawar <pawar.rani241996@gmail.com>
2024-10-20 13:15:20 +0530

    Update question1.java

--- DSA1/Array/problems/question1.java
+++ DSA1/Array/problems/question1.java
@@ -7,10 +7,14 @@ public class question1 {
         int a = 9;
         int b = 3;
         int[] arr = { 1, 2, 3, 4, 5 };
+        System.out.println swap(a, b);
 
     }
 
     public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
 
     }
 
