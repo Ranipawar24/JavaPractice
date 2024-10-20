cd1498b227098785274c0b5785811afc8d689a0e
Rani Pawar <pawar.rani241996@gmail.com>
2024-10-20 13:05:56 +0530

    Update question1.java

--- DSA1/Array/problems/question1.java
+++ DSA1/Array/problems/question1.java
@@ -3,4 +3,8 @@ package DSA1.Array.problems;
 public class question1 {
     // 1. Given 2 integers a and b. swap the given values using temporary variables?
 
+    public static void main(String[] args) {
+
+    }
+
 }
