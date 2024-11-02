adaa790f2747e057bb06ba6f29df9164da27e30c
Rani Pawar <pawar.rani241996@gmail.com>
2024-11-02 13:22:38 +0530

    Update question2.java

--- DSA1/Array/problems/question2.java
+++ DSA1/Array/problems/question2.java
@@ -4,7 +4,7 @@ public class question2 {
     // 2. Given 2 integers a and b. swap the 2 given values using sum and difference
     // method?
     public static void main(String[] args) {
-        //
+        
 
     }
 
