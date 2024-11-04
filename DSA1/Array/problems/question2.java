7cc768304eb118390a2e01db76bd2d866627d8c1
Rani Pawar <pawar.rani241996@gmail.com>
2024-11-04 11:38:23 +0530

    Update question2.java

--- DSA1/Array/problems/question2.java
+++ DSA1/Array/problems/question2.java
@@ -4,10 +4,24 @@ public class question2 {
     // 2. Given 2 integers a and b. swap the 2 given values using sum and difference
     // method?
     public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        int[] arr = { 1, 2, 3, 4, 5 };
+        swapWithoutTemp(a, b);
 
     }
 
     static void swapWithoutTemp(int a, int b) {
+        System.out.println("Values before swap");
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Values After swap");
+        System.out.println("a:" + a);
+        System.out.println("b:" + b);
 
     }
 
