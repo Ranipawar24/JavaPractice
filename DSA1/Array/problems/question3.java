d5aa0bc547b8569653dc4eed1443127395db87c2
Rani Pawar <pawar.rani241996@gmail.com>
2024-11-11 10:05:18 +0530

    Update question3.java

--- DSA1/Array/problems/question3.java
+++ DSA1/Array/problems/question3.java
@@ -9,12 +9,18 @@ public class question3 {
 
     }
 
-    public static void reverseArray(int arr[]) {
+    public static int[] reverseArray(int arr[]) {
         int n = arr.length;
         int ans[] = new int[n];
         int j = 0;
 
         // traverse oroginal array in reverse direction
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[j++] = arr[i];
+
+        }
+        return ans;
 
     }
 
