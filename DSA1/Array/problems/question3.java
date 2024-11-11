8cbe9ef3a5250203dac519100aef02ee9677c93e
Rani Pawar <pawar.rani241996@gmail.com>
2024-11-11 10:06:51 +0530

    Update question3.java

--- DSA1/Array/problems/question3.java
+++ DSA1/Array/problems/question3.java
@@ -5,7 +5,8 @@ public class question3 {
     // Reverse an array consisting of integer values?
 
     public static void main(String[] args) {
-        int ans[] = { 2, 3, 5, 4, 6 };
+        int arr[] = { 2, 3, 5, 4, 6 };
+        int ans[] = reverseArray(arr);
 
     }
 
