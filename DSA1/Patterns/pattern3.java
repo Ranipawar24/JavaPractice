4839197cf7ce6a5b17ce52551153aee316168b88
Rani Pawar <pawar.rani241996@gmail.com>
2025-01-13 12:54:56 +0530

    Update pattern3.java

--- DSA1/Patterns/pattern3.java
+++ DSA1/Patterns/pattern3.java
@@ -2,6 +2,12 @@ package DSA1.Patterns;
 
 public class pattern3 {
     public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+
+            }
+        }
 
     }
 }
