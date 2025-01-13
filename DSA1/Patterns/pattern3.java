80ced48425680ea43bc28685ffe3c6fdf856db73
Rani Pawar <pawar.rani241996@gmail.com>
2025-01-13 12:57:40 +0530

    Update pattern3.java

--- DSA1/Patterns/pattern3.java
+++ DSA1/Patterns/pattern3.java
@@ -6,12 +6,14 @@ public class pattern3 {
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= n; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.println("*");
+                    System.out.print("*");
                 } else {
+                    System.out.print(" ");
 
                 }
 
             }
+
         }
 
     }
