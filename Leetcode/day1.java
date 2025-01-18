6ceca47da8b5f4e8cc79265abdfcc051410984ce
Rani Pawar <pawar.rani241996@gmail.com>
2025-01-18 11:14:59 +0530

    Update day1.java

--- Leetcode/day1.java
+++ Leetcode/day1.java
@@ -4,6 +4,10 @@ public class day1 {
     public static void main(String[] args) {
 
         public boolean isPalindrome(int x) {
+            if(x<0){
+                return false;
+    
+            }
             
     
         }
