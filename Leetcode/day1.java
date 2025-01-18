99c1f6a60b05b2cc9ad281585b3a3619b2f88c54
Rani Pawar <pawar.rani241996@gmail.com>
2025-01-18 11:15:36 +0530

    Update day1.java

--- Leetcode/day1.java
+++ Leetcode/day1.java
@@ -8,7 +8,14 @@ public class day1 {
                 return false;
     
             }
-            
+            int n=x;
+            int revNum = 0;
+            while(n>0){
+                int digit =n%10;
+                revNum =revNum*10+digit;
+                n=n/10;
+    
+            }
     
         }
 
