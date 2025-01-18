ca630ead604bcc1ddfa0ca43dfff18275bba2af7
Rani Pawar <pawar.rani241996@gmail.com>
2025-01-18 11:38:38 +0530

    Update day1.java

--- Leetcode/day1.java
+++ Leetcode/day1.java
@@ -1,6 +1,12 @@
 package Leetcode;
 
 public class day1 {
+    public static void main(String[] args) {
+        day1 obj = new day1(); // Create an object to call the isPalindrome method
+        int testNumber = 121; // Example number to test
+        boolean result = obj.isPalindrome(testNumber); // Call the method
+        System.out.println(result); // Print the result
+    }
 
     public boolean isPalindrome(int x) {
         if (x < 0) {
@@ -22,5 +28,4 @@ public class day1 {
         }
 
     }
-
 }
