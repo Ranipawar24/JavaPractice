78489c10444edfe2251d9f1d3ed6b31c65f7ca03
Rani Pawar <pawar.rani241996@gmail.com>
2025-02-15 21:07:07 +0530

    Update day3.java

--- Leetcode/day3.java
+++ Leetcode/day3.java
@@ -2,8 +2,14 @@ package Leetcode;
 
 public class day3 {
     // Q 3.Fibonacci Number ?
-    public static void main(String[] args) {
-        
+
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
     }
 
 }
